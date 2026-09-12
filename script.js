984dedddb0df4c263a632f3ae903cf5b61d83e95
Josh14 <joshua.huizer@gmail.com>
2026-09-12 14:41:44 +0200

    Small fix

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ if (balance === null){
     document.getElementById("jsSection").innerHTML = '<h2>First time setup</h2><input type="number" placeholder="Enter starting balance" id=balanceInput><br><input placeholder="Enter currency symbol" id=currencyInput><br><button id=continueButton>Continue</button>';
     document.getElementById("continueButton").addEventListener('click', () => {
         balance = document.getElementById("balanceInput").value
-        currency = document.getElementById("currencyInput")
+        currency = document.getElementById("currencyInput").value
         document.getElementById("jsSection").innerHTML = '<h2>Enter goal</h2><input placeholder="Enter goal name" id="goal1name"><br><label for="goal1price">Enter goal price:<br>' + currency + '<input type="number" id="goal1price"><br><button id=continueButton>Done!</button>';
     })
 }
